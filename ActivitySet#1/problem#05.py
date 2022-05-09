66d85332ee4f697c1cfe26d4ad45eb29992f29ed
AbdullahZaidPPS <abdullazaid820@gmail.com>
2022-05-09 10:13:19 +0530

    9/5 p5

--- ActivitySet#1/problem#05.py
+++ ActivitySet#1/problem#05.py
@@ -1,12 +1,15 @@
 # Functions
+def calcpay(h,r):
+ if h>40:
+    sly = 1.5*(h-40)*r+(40*r)
+    print("The salary is {}".format(sly))
+    return sly
+ else:
+    sly = h*r
+    print("The salary is {}".format(sly))
+    return sly
 
+hours = float(input("enter hours \n"))
+rate = float(input("enter hourly rate \n"))
+gpay = calcpay(hours,rate)
 
-def computepay(h, r):
-    pass  # ...
-
-
-hrs = float(input("Enter hours? "))
-rte = float(input("Enter rate per hour? "))
-
-p = computepay(hrs, rte)
-print("Pay", p)
