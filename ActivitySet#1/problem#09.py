323ae4e4e66d7f065da7be7a383e2f7c06ab42cb
AbdullahZaidPPS <abdullazaid820@gmail.com>
2022-05-12 22:31:00 +0530

    12/5

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,19 @@
 # Lists
 
 filename = "dataset/romeo.txt"
+
+def lists2():
+    
+    file=open(filename)
+    linesall=list()
+    for line in file:
+        words=line.split()
+        for k in words:
+            if k in linesall:
+                continue
+            else:
+                linesall.append(k)
+    linesall.sort()
+    print(linesall)
+lists2()
+            
