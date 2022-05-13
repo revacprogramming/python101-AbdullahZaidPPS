010761d2af1d5703a1485bb863f788c590b7c2e2
AbdullahZaidPPS <abdullazaid820@gmail.com>
2022-05-13 23:18:58 +0530

    13/5

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,3 +1,17 @@
 # Dictionaries
 
-filename = "dataset/mbox-short.txt"
+#filename = "dataset/mbox-short.txt"
+fh = open(fname)
+count = 0
+for line in fh:
+    if not line.startswith("From"):
+        continue
+    if line.startswith("From:"):
+        continue
+    if line.startswith("From"):
+        words=line.split()
+        print(words[1])
+        count=count+1
+        
+
+
