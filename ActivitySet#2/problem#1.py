30230ad6e38f1614c760c05f5279a1610c6d5e20
Abdullah M Zaid <95867207+AbdullahZaidPPS@users.noreply.github.com>
2022-05-15 17:51:22 +0000

    15/5

--- ActivitySet#2/problem#1.py
+++ ActivitySet#2/problem#1.py
@@ -1,12 +1,12 @@
 
 
 def add(a, b):
-    return  # ...
+  c=a+b
+  return c
 
 
 def main():
-    a = input  # ...
-    b = input  # ...
-
-    c = add(a, b)
-    print  # ...
+    a = float(input("Enter number"))
+    b = float(input("Enter number"))
+    sum = add(a, b)
+    print("The sum of %f and %f is %f".format(a,b,sum))
